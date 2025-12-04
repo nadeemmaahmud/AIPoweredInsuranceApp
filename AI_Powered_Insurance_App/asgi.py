3e9f985ffe236c7cf49d3d0c9ccccec550ac4c72
nadeemmaahmud <nadeemmaahmud@gmail.com>
2025-12-05 05:05:32 +0600

    Consumer configured for premium chat

--- AI_Powered_Insurance_App/asgi.py
+++ AI_Powered_Insurance_App/asgi.py
@@ -8,12 +8,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
 django_asgi_app = get_asgi_application()
 
 from generalchat.routing import websocket_urlpatterns as chat_patterns
+from premiumchat.routing import websocket_urlpatterns as premium_chat_patterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": JWTAuthMiddleware(
         URLRouter(
-            chat_patterns
+            chat_patterns + premium_chat_patterns
         )
     ),
 })
